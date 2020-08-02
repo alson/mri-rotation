28614090fced96f473e2da30fb95d34454b1b114
Alson van der Meulen <git@alm.nufan.net>
2020-08-02 09:31:06 +0200

    Change to L2 loss function and add metrics.

--- model/loss.py
+++ model/loss.py
@@ -3,3 +3,7 @@ import torch.nn.functional as F
 
 def l1_loss(output, target):
     return F.l1_loss(output[:, 0], target)
+
+
+def mse_loss(output, target):
+    return F.mse_loss(output[:, 0], target)
--- model/metric.py
+++ model/metric.py
@@ -1,4 +1,7 @@
+from math import sqrt
+
 import torch
+
 
 def mean_absolute_error(output, target):
     with torch.no_grad():
@@ -6,3 +9,19 @@ def mean_absolute_error(output, target):
         assert pred.shape[0] == len(target)
         sum_abs_err = torch.sum(torch.abs(pred - target))
     return sum_abs_err / len(target)
+
+
+def mean_squared_error(output, target):
+    with torch.no_grad():
+        pred = output[:, 0]
+        assert pred.shape[0] == len(target)
+        sum_sq_err = torch.sum(torch.square(pred - target))
+    return sum_sq_err / len(target)
+
+
+def root_mean_squared_error(output, target):
+    with torch.no_grad():
+        pred = output[:, 0]
+        assert pred.shape[0] == len(target)
+        sum_sq_err = torch.sum(torch.square(pred - target))
+    return sqrt(sum_sq_err / len(target))
