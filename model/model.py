1cf220220746e80f89f91b28cb4252771171acdb
Alson van der Meulen <git@alm.nufan.net>
2020-08-08 23:08:53 +0200

    Switch to ResNet-50. Decrease learning rate.

--- model/model.py
+++ model/model.py
@@ -2,10 +2,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
 from torchvision.models import ResNet
-from torchvision.models.resnet import BasicBlock
+from torchvision.models.resnet import BasicBlock, Bottleneck
 
 
-class AdniModel(ResNet):
+class AdniModel34(ResNet):
+    def __init__(self, **kwargs):
+        super().__init__(BasicBlock,  [2, 2, 2, 2], **kwargs)
+        self.fc = nn.Linear(512 * BasicBlock.expansion, 2)
+
+
+class AdniModel34(ResNet):
     def __init__(self, **kwargs):
         super().__init__(BasicBlock, [3, 4, 6, 3], **kwargs)
         self.fc = nn.Linear(512 * BasicBlock.expansion, 2)
+
+
+class AdniModel50(ResNet):
+    def __init__(self, **kwargs):
+        super().__init__(Bottleneck, [3, 4, 6, 3], **kwargs)
+        self.fc = nn.Linear(512 * Bottleneck.expansion, 2)
