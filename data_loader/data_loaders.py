6733fb7fdf634665704f602bc1ff934ad10cfe77
Alson van der Meulen <git@alm.nufan.net>
2020-08-14 23:59:28 +0200

    Augment by center cropping. Decrease learning rate.

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -26,16 +26,19 @@ class AdniDataLoader(BaseDataLoader):
     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
         if training:
             trsfm = transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.Lambda(lambda x: TF.center_crop(x, [x.width * uniform(0.8, 1)] * 2)),
+                transforms.Lambda(lambda x: TF.resize(x, [100, 100])),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5,), (0.5,)),
                 transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
                 # transforms.Lambda(lambda x: TF.adjust_contrast(x, contrast_factor=uniform(0.95, 1.05))),
                 # transforms.Lambda(lambda x: TF.adjust_brightness(x, brightness_factor=uniform(0.95, 1.05))),
-                # transforms.Lambda(lambda x: TF.center_crop(x, [x.shape[-2:][::-1][0] * uniform(0.75, 1)] * 2)),
-                # transforms.Lambda(lambda x: TF.resize(x, [100, 100])),
             ])
         else:
             trsfm = transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.Lambda(lambda x: TF.resize(x, [100, 100])),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5,), (0.5,)),
                 transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
